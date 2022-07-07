6bc38a7e2eb957617eb44a9a32ed77eacab4f4f2
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-07 11:19:43 -0400

    added crud to movieDAO

--- src/main/java/com/revature/dao/MovieDAO.java
+++ src/main/java/com/revature/dao/MovieDAO.java
@@ -1,5 +1,50 @@
 package com.revature.dao;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.models.Movie;
+
+import com.revature.util.HibernateUtility;
+
 public class MovieDAO {
 
+	public void insertWIhtoutCasting(Movie movie) {
+
+		Session s = HibernateUtility.getSession();
+		Transaction tr = s.beginTransaction();
+		s.save(movie); 
+		tr.commit();
+
+	}
+			
+	public int insert(Movie movie) {
+		Session s = HibernateUtility.getSession();
+		Transaction tr = s.beginTransaction();
+		int pk = (int) s.save(movie);
+		tr.commit();
+		return pk;
+	}
+	
+	public List<Movie> selectAll(){
+		Session s = HibernateUtility.getSession();
+		return s.createQuery("from Movie", Movie.class).list();
+	}
+	
+	public Movie selectByName(String name) {
+		Session s = HibernateUtility.getSession();
+		Movie movie = (Movie) s.createQuery("from Movie where name = '" +name+"'", Movie.class);
+		return movie;
+	}
+	
+	public Movie selectById(int id) {
+		Session s = HibernateUtility.getSession();
+		return s.get(Movie.class, id);
+	}
+	
+	
+	
+	
 }
