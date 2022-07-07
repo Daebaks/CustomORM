fe9eebbe2aebb2a0d8b3ffdec2b942eaf14cc132
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-06 20:49:58 -0400

    implemented customer, ticket, and movie models and all joins

--- src/main/java/com/revature/models/Movie.java
+++ src/main/java/com/revature/models/Movie.java
@@ -1,12 +1,16 @@
 package com.revature.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -33,105 +37,9 @@ public class Movie {
 	@Column(name = "movie_release_year", nullable = false)
 	private int releasedYear;
 
-	public int getMovieId() {
-		return movieId;
-	}
+	@OneToMany(mappedBy = "movieTicket", fetch = FetchType.LAZY)
+	private List<Ticket> tickets = new ArrayList<>();
 
-	public void setMovieId(int movieId) {
-		this.movieId = movieId;
-	}
-
-	public String getMovieTitle() {
-		return movieTitle;
-	}
-
-	public void setMovieTitle(String movieTitle) {
-		this.movieTitle = movieTitle;
-	}
-
-	public String getMovieType() {
-		return movieType;
-	}
-
-	public void setMovieType(String movieType) {
-		this.movieType = movieType;
-	}
-
-	public int getMovieDurationMin() {
-		return movieDurationMin;
-	}
-
-	public void setMovieDurationMin(int movieDurationMin) {
-		this.movieDurationMin = movieDurationMin;
-	}
-
-	public double getTicketPrice() {
-		return ticketPrice;
-	}
-
-	public void setTicketPrice(double ticketPrice) {
-		this.ticketPrice = ticketPrice;
-	}
-
-	public int getReleasedYear() {
-		return releasedYear;
-	}
-
-	public void setReleasedYear(int releasedYear) {
-		this.releasedYear = releasedYear;
-	}
-
-	public Movie() {
-		super();
-	}
-
-	public Movie(int movieId, String movieTitle, String movieType, int movieDurationMin, double ticketPrice,
-			int releasedYear) {
-		super();
-		this.movieId = movieId;
-		this.movieTitle = movieTitle;
-		this.movieType = movieType;
-		this.movieDurationMin = movieDurationMin;
-		this.ticketPrice = ticketPrice;
-		this.releasedYear = releasedYear;
-	}
-
-	public Movie(String movieTitle, String movieType, int movieDurationMin, double ticketPrice, int releasedYear) {
-		super();
-		this.movieTitle = movieTitle;
-		this.movieType = movieType;
-		this.movieDurationMin = movieDurationMin;
-		this.ticketPrice = ticketPrice;
-		this.releasedYear = releasedYear;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(movieDurationMin, movieId, movieTitle, movieType, releasedYear, ticketPrice);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Movie other = (Movie) obj;
-		return movieDurationMin == other.movieDurationMin && movieId == other.movieId
-				&& Objects.equals(movieTitle, other.movieTitle) && Objects.equals(movieType, other.movieType)
-				&& releasedYear == other.releasedYear
-				&& Double.doubleToLongBits(ticketPrice) == Double.doubleToLongBits(other.ticketPrice);
-	}
-
-	@Override
-	public String toString() {
-		return "Movie [movieId=" + movieId + ", movieTitle=" + movieTitle + ", movieType=" + movieType
-				+ ", movieDurationMin=" + movieDurationMin + ", ticketPrice=" + ticketPrice + ", releasedYear="
-				+ releasedYear + "]";
-	}
-	
 	
 	
 }
---
+++ src/main/java/com/revature/models/Theatre.java
@@ -0,0 +1,10 @@
+package com.revature.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "theatre")
+public class Theatre {
+
+}
