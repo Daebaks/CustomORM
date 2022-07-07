53f56b0d5d03025923a2d7908fbbf83adcfd3915
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-06 20:57:49 -0400

    joined all relationships and theatre/movie

--- src/main/java/com/revature/models/Movie.java
+++ src/main/java/com/revature/models/Movie.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -40,6 +43,8 @@ public class Movie {
 	@OneToMany(mappedBy = "movieTicket", fetch = FetchType.LAZY)
 	private List<Ticket> tickets = new ArrayList<>();
 
-	
+	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "movie_fk")
+	private List<Theatre> theatreHolder;
 	
 }
--- src/main/java/com/revature/models/Theatre.java
+++ src/main/java/com/revature/models/Theatre.java
@@ -1,10 +1,36 @@
 package com.revature.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "theatre")
 public class Theatre {
 
+	@Id
+	@Column(name = "theatre_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int theatreId;
+	
+	@Column(name = "num_seats", nullable = false)
+	private int numOfSeats;
+	
+	@Column(name = "curr_movie", nullable = false)
+	private String currMovie;
+	
+	@Column(name = "movie_menu", nullable = false)
+	private List<Movie> movieMenu;
+	
+	@ManyToMany(mappedBy = "theatreHolder", fetch = FetchType.LAZY)
+	private List<Movie> movies = new ArrayList<>();
+	
 }
