3fccf1fad9b6916400702f53dcde4ed192a73a1c
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-06 21:00:24 -0400

    getters+setters+constructors for theatre

--- src/main/java/com/revature/models/Theatre.java
+++ src/main/java/com/revature/models/Theatre.java
@@ -2,6 +2,7 @@ package com.revature.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -32,5 +33,93 @@ public class Theatre {
 	
 	@ManyToMany(mappedBy = "theatreHolder", fetch = FetchType.LAZY)
 	private List<Movie> movies = new ArrayList<>();
+
+	public int getTheatreId() {
+		return theatreId;
+	}
+
+	public void setTheatreId(int theatreId) {
+		this.theatreId = theatreId;
+	}
+
+	public int getNumOfSeats() {
+		return numOfSeats;
+	}
+
+	public void setNumOfSeats(int numOfSeats) {
+		this.numOfSeats = numOfSeats;
+	}
+
+	public String getCurrMovie() {
+		return currMovie;
+	}
+
+	public void setCurrMovie(String currMovie) {
+		this.currMovie = currMovie;
+	}
+
+	public List<Movie> getMovieMenu() {
+		return movieMenu;
+	}
+
+	public void setMovieMenu(List<Movie> movieMenu) {
+		this.movieMenu = movieMenu;
+	}
+
+	public List<Movie> getMovies() {
+		return movies;
+	}
+
+	public void setMovies(List<Movie> movies) {
+		this.movies = movies;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(currMovie, movieMenu, movies, numOfSeats, theatreId);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Theatre other = (Theatre) obj;
+		return Objects.equals(currMovie, other.currMovie) && Objects.equals(movieMenu, other.movieMenu)
+				&& Objects.equals(movies, other.movies) && numOfSeats == other.numOfSeats
+				&& theatreId == other.theatreId;
+	}
+
+	@Override
+	public String toString() {
+		return "Theatre [theatreId=" + theatreId + ", numOfSeats=" + numOfSeats + ", currMovie=" + currMovie
+				+ ", movieMenu=" + movieMenu + ", movies=" + movies + "]";
+	}
+
+	public Theatre(int theatreId, int numOfSeats, String currMovie, List<Movie> movieMenu, List<Movie> movies) {
+		super();
+		this.theatreId = theatreId;
+		this.numOfSeats = numOfSeats;
+		this.currMovie = currMovie;
+		this.movieMenu = movieMenu;
+		this.movies = movies;
+	}
+
+	public Theatre(int numOfSeats, String currMovie, List<Movie> movieMenu, List<Movie> movies) {
+		super();
+		this.numOfSeats = numOfSeats;
+		this.currMovie = currMovie;
+		this.movieMenu = movieMenu;
+		this.movies = movies;
+	}
+
+	public Theatre() {
+		super();
+	}
+	
+	
 	
 }
