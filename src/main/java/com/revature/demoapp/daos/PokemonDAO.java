873bd790c0810cc2555558b4a3c20b20bff15798
ali alrubaye <alialrubaye1990@hotmail.com>
2022-07-11 00:39:34 -0400

    starting ORM crud management implementation

--- src/main/java/com/revature/demoapp/daos/PokemonDAO.java
+++ src/main/java/com/revature/demoapp/daos/PokemonDAO.java
@@ -1,13 +1,13 @@
 package com.revature.demoapp.daos;
 
-import com.revature.demoapp.models.Pokemon;
+
 
 public class PokemonDAO {
 
+	
 	//for testing
-	public int insert(Pokemon pokemon) {
-		
-		
+	public int insert() {
+		//for the inserting a pokemon. Needs insertInDb() from session.
 		return -1;
 	}
 	
